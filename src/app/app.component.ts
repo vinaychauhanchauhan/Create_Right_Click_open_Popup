daf310680e65a730a420bb2e8d62916b7042ee10
vinaychauhanchauhan <ramvinayc33@gmail.com>
2024-07-12 23:07:09 +0530

    send

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
+
   title = 'Create_Right_Click_open_Popup';
+
+  @ViewChild('contextMenu') contextMenu!: MatMenu;
+  @ViewChild(MatMenuTrigger) trigger!: MatMenuTrigger;
+
+  menuPosition = { x: '0px', y: '0px' };
+
+  onRightClick(event: MouseEvent) {
+  
+    event.preventDefault();
+    this.menuPosition.x = event.clientX + 'px';
+    this.menuPosition.y = event.clientY + 'px';
+
+   // this.trigger.menuData = { x: this.menuPosition.x, y: this.menuPosition.y };
+    this.trigger.openMenu(); // Open the context menu
+   // this.contextMenu.openMenu(); // Open the context menu
+  }
+
+
+  onAction(action: string) {
+    console.log(`Selected: ${action}`);
+    // Add action logic here
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModalComponent } from './modal/modal.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,  // Required for animations
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
